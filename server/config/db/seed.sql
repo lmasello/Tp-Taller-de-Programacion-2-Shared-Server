65b1b7e0bb9f3462b257195f1bb94736b7f2903c
German Guzelj <german.guzelj@almundo.com>
2017-03-28 09:38:02 -0300

    Fixing seed.sql

--- server/config/db/seed.sql
+++ server/config/db/seed.sql
@@ -7,6 +7,6 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (email, first_name, last_name, password)
-  VALUES ('maselloleandro@gmail.com', 'Leandro', 'Masello', '"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"');
+  VALUES ('maselloleandro@gmail.com', 'Leandro', 'Masello', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
 INSERT INTO users (email, first_name, last_name, password)
-  VALUES ('gguzelj@gmail.com', 'German', 'Guzelj', '"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"');
+  VALUES ('gguzelj@gmail.com', 'German', 'Guzelj', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
